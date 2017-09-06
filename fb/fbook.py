cdac9df727aca5c343c952250b17003f44e01f90
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-09-07 02:11:55 +0900

    Update fbook.py

--- fb/fbook.py
+++ fb/fbook.py
@@ -3,9 +3,9 @@ import json
 import requests
 from util import Utils
 
-APP_ID = '1904193739907094'
-APP_SECRET = 'c5eaa6d673acd89bf037e32210e77e01'
-ACCESS_TOKEN = 'EAACEdEose0cBAOGaGTaC7MwvPGV9wIXG3xbosZBogcXxaZCggMSiSCJF1YijQqhf0ULZAGr3CN2GFSwZBTgPraiMV8qxKxWgK5ScDX4SwX7k9gfhhgZAxeSXc1cEiC0NSOPMcYHnzkLJDGJ5MgIcVGsJ6aic2sINcxKYyPPr33vcdeuec5GJpfjxGV8WzDZCJKB7Hk8sPJXgZDZD'
+APP_ID = ''
+APP_SECRET = ''
+ACCESS_TOKEN = ''
 
 
 def pp(material):
